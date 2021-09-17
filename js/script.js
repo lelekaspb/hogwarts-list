57ed4a1bab2cb76b2124f018b32061f37a564f73
lelekaspb <bov-1986@yandex.ru>
2021-09-17 22:38:05 +0200

    divided one function to few

--- js/script.js
+++ js/script.js
@@ -1,35 +1,59 @@
 "use strict";
 
+const reference = {
+  prefect: {
+    appoint: "Appoint as a prefect",
+    revoke: "Revoke as a prefect",
+  },
+  squad: {
+    add: "Add to inquisitorial squad",
+    remove: "Remove from inquisitorial squad",
+  },
+};
+
 document.querySelector("#test").addEventListener("click", fadeOut);
 
 function fadeOut(e) {
   if (e.target.classList.contains("prefect")) {
     e.target.querySelector("span").classList.add("fade_out");
-    e.target.querySelector("span").addEventListener("animationend", changeText);
+    e.target
+      .querySelector("span")
+      .addEventListener("animationend", changeButton);
   } else {
     e.target.classList.add("fade_out");
-    e.target.addEventListener("animationend", changeText);
+    e.target.addEventListener("animationend", changeButton);
   }
 }
 
-function changeText(e) {
+function changeButton(e) {
   if (e.target.classList.contains("prefect")) {
-    e.target.querySelector("span").textContent = "Revoke as a prefect";
-    e.target.querySelector("span").classList.remove("fade_out");
-    e.target
-      .querySelector("span")
-      .removeEventListener("animationend", changeText);
-    e.target.querySelector("span").classList.add("fade_in");
-    e.target
-      .querySelector("span")
-      .addEventListener("animationend", removeFadeIn);
+    changeDataAttribute(
+      e.target.querySelector("span"),
+      e.target.querySelector("span").dataset.prefect
+    );
+    e.target.querySelector("span").textContent =
+      reference.prefect[e.target.querySelector("span").dataset.prefect];
+    changeAnimation(e.target.querySelector("span"));
   } else {
-    e.target.removeEventListener("animationend", changeText);
-    e.target.classList.remove("fade_out");
-    e.target.textContent = "Revoke as a prefect";
-    e.target.classList.add("fade_in");
-    e.target.addEventListener("animationend", removeFadeIn);
+    changeDataAttribute(e.target, e.target.dataset.prefect);
+    e.target.textContent = reference.prefect[e.target.dataset.prefect];
+    changeAnimation(e.target);
   }
+
+  function changeDataAttribute(element, attr) {
+    if (attr === "appoint") {
+      element.dataset.prefect = "revoke";
+    } else if (attr === "revoke") {
+      element.dataset.prefect = "appoint";
+    }
+  }
+}
+
+function changeAnimation(element) {
+  element.classList.remove("fade_out");
+  element.removeEventListener("animationend", changeButton);
+  element.classList.add("fade_in");
+  element.addEventListener("animationend", removeFadeIn);
 }
 
 function removeFadeIn(e) {
